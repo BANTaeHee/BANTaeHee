f987971e2e0b6b3810a2f0a79ff927045c57d762
BANTaeHee <ban7598@gmail.com>
2023-03-18 15:58:07 +0900

    2023-03-18 JavaPractice #4

---
+++ CodingPractice/src/Chap_04/_06_While.java
@@ -0,0 +1,26 @@
+package Chap_04;
+
+public class _06_While {
+
+	public static void main(String[] args) {
+		// 반복문 while
+		// 수영장에서 수영하는 모습
+		int distance = 25;	// 전체 거리 25m
+		int move = 0;	// 현재 이동 거리 0m
+		while (move < distance) {	// 현재 이동 거리가 전체 거리보다 작다는 조건이 참인 동안 반복 수행
+			System.out.println("발차기를 계속합니다.");
+			System.out.println("현재 이동 거리 : " + move);
+			move += 3;	// 3미터 이동
+		}
+		System.out.println("도착하였습니다.");
+		
+		// 무한 루프
+		move = 0;
+		while (move < distance) {	// 현재 이동 거리가 전체 거리보다 작다는 조건이 참인 동안 반복 수행
+			System.out.println("발차기를 계속합니다.");
+			System.out.println("현재 이동 거리 : " + move);
+		}
+		System.out.println("도착하였습니다.");
+	}
+
+}
