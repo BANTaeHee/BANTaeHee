b0d2d7dcc8fd4a585e31197c3cfb689d572c689a
BANTaeHee <ban7598@gmail.com>
2023-04-05 20:35:41 +0900

    2023-04-05 JavaPractice #4

---
+++ CodingPractice/src/Chap_04/_09_MultipleTable.java
@@ -0,0 +1,18 @@
+package Chap_04;
+
+public class _09_MultipleTable {
+
+	public static void main(String[] args) {
+		// 구구단
+		// 이중 반복문
+		
+		for (int i = 2; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {
+				System.out.println(i + " * " + j + " = " + (i * j));
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
